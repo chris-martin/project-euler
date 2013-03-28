710dd3b049305cf5dee1e39a81fac94373890db1
Christopher Martin <ch.martin@gmail.com>
2013-03-27 22:55:13 -0400

    problem 9

---
+++ 9/src/main/scala/main.scala
@@ -0,0 +1,12 @@
+object Main {
+  def main(args: Array[String]) {
+
+    println(
+      (for (a <- (0 to 1000).toStream; b <- ((a +1) to 1000).toStream) yield {
+        val c = 1000 - (a + b)
+        if (b < c && a*a + b*b == c*c) Some(Seq(a, b, c)) else None
+      }).view.flatten.head.product
+    )
+
+  }
+}
