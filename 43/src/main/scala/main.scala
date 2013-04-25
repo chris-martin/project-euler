5ad80a058036e5caa8291b775aadad08145ab371
Christopher Martin <ch.martin@gmail.com>
2013-04-25 00:10:06 -0400

    problem 43

---
+++ 43/src/main/scala/main.scala
@@ -0,0 +1,16 @@
+object Main extends App {
+  println(
+    (0 to 9).permutations.map(_.mkString)
+      .filter({ s =>
+        s.substring(1, 4).toInt % 2 == 0 &&
+        s.substring(2, 5).toInt % 3 == 0 &&
+        s.substring(3, 6).toInt % 5 == 0 &&
+        s.substring(4, 7).toInt % 7 == 0 &&
+        s.substring(5, 8).toInt % 11 == 0 &&
+        s.substring(6, 9).toInt % 13 == 0 &&
+        s.substring(7, 10).toInt % 17 == 0
+      })
+      .map(_.toLong)
+      .sum
+  )
+}
