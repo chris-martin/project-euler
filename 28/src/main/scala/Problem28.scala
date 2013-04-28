768fda41f154d4bd4581034eada0eeaac3972796
Christopher Martin <ch.martin@gmail.com>
2013-04-28 04:08:41 -0400

    refactor 28

--- 28/src/main/scala/Problem28.scala
+++ 28/src/main/scala/Problem28.scala
@@ -1,14 +1,13 @@
-object Main {
+object Problem28 extends App {
 
-  def main(args: Array[String]) {
+  println(answer)
 
-    println(1 + (1 to 500).flatMap(i => {
+  def answer: Int =
+    1 + (1 to 500).flatMap(i => {
       val j = 2*i
       val x = square(j+1)
       Seq(x, x-j, x-2*j, x-3*j)
-    }).sum)
-
-  }
+    }).sum
 
   def square(n: Int): Int = n*n
 
---
+++ 28/src/test/scala/Problem28Test.scala
@@ -0,0 +1,9 @@
+class Problem28Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(669171001) {
+      Problem28.answer
+    }
+  }
+
+}
