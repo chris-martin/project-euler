527e6158c70d904c772e458042f35e3d61d75552
Christopher Martin <ch.martin@gmail.com>
2013-04-28 03:51:53 -0400

    refactor 26

--- 26/src/main/scala/main.scala
+++ 26/src/main/scala/main.scala
@@ -1,37 +1,38 @@
-object Main {
+object Problem26 extends App {
 
-  def main(args: Array[String]) {
-    println((2 to 999).map(Fraction(1, _)).maxBy(_.period))
-  }
+  println(answer)
 
-}
+  def answer: Int =
+    (2 to 999).map(Fraction(1, _)).maxBy(_.period).d.toInt
 
-case class Fraction(c: BigInt, d: BigInt) {
+  case class Fraction(c: BigInt, d: BigInt) {
 
-  lazy val period: Int = {
+    lazy val period: Int = {
 
-    /*
-      This algorithm only works for c=1.
-      It relies on the fact that the period of 1/d is less than d
-     */
-    assert(c == 1)
+      /*
+        This algorithm only works for c=1.
+        It relies on the fact that the period of 1/d is less than d
+       */
+      assert(c == 1)
 
-    assert(d.isValidInt)
+      assert(d.isValidInt)
 
-    (
-      Stream.from(0) flatMap { i =>
-        (1 until d.toInt) find { period =>
-          shiftLeft(i).remainder.reduce == shiftLeft(i+period).remainder.reduce
+      (
+        Stream.from(0) flatMap { i =>
+          (1 until d.toInt) find { period =>
+            shiftLeft(i).remainder.reduce == shiftLeft(i+period).remainder.reduce
+          }
         }
-      }
-    ).head
+      ).head
+
+    }
+
+    def remainder: Fraction = Fraction(c mod d, d)
+    def shiftLeft(n: Int): Fraction = *(BigInt(10) pow n)
+    def *(x: BigInt): Fraction = Fraction(c*x, d)
+    def reduce: Fraction = { val x = c gcd d; Fraction(c/x, d/x) }
+    override def toString: String = "%s/%s".format(c, d)
 
   }
-
-  def remainder: Fraction = Fraction(c mod d, d)
-  def shiftLeft(n: Int): Fraction = *(BigInt(10) pow n)
-  def *(x: BigInt): Fraction = Fraction(c*x, d)
-  def reduce: Fraction = { val x = c gcd d; Fraction(c/x, d/x) }
-  override def toString: String = "%s/%s".format(c, d)
 
 }
---
+++ 26/src/test/scala/Problem26Test.scala
@@ -0,0 +1,9 @@
+class Problem26Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(983) {
+      Problem26.answer
+    }
+  }
+
+}
