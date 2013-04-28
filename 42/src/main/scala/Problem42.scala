a1b6595aa967dcc38ea6e80941258ec045d12354
Christopher Martin <ch.martin@gmail.com>
2013-04-28 05:12:35 -0400

    refactor 42

--- 42/src/main/scala/Problem42.scala
+++ 42/src/main/scala/Problem42.scala
@@ -1,11 +1,13 @@
-object Main extends App {
+object Problem42 extends App {
 
-  val triangles = Stream.from(1).map(n => n*(n+1)/2)
+  println(answer)
 
-  println(
+  lazy val triangles = Stream.from(1).map(n => n*(n+1)/2)
+
+  def answer: Int =
     io.Source.fromURL(getClass.getResource("words.txt"))
       .mkString.replace("\"", "").split(",")
       .map(_.toUpperCase.map(_-'A'+1).sum)
       .count(v => triangles.takeWhile(_ <= v).contains(v))
-  )
+
 }
---
+++ 42/src/test/scala/Problem42Test.scala
@@ -0,0 +1,9 @@
+class Problem42Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(162) {
+      Problem42.answer
+    }
+  }
+
+}
