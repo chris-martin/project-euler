410753a3bab1228f259e4d56560c56f5b9b3aa9c
Christopher Martin <ch.martin@gmail.com>
2013-04-28 04:04:14 -0400

    refactor 27

---
+++ 27/src/main/scala/Problem27.scala
@@ -0,0 +1,31 @@
+object Problem27 extends App {
+
+  println(answer)
+
+  def answer: Int = {
+
+    val expressions =
+      for {
+        a <- (-999 to 999)
+        b <- (-999 to 999)
+      }
+      yield
+        new QuadExpression(a, b)
+
+    val best = expressions.maxBy(_.nrOfPrimes)
+
+    best.a * best.b
+  }
+
+  case class QuadExpression(a: Int, b: Int) {
+
+    def apply(n: Int): Int = n*n + a*n + b
+
+    lazy val nrOfPrimes: Int =
+      Stream.from(0)
+        .takeWhile(n => BigInt(apply(n)).isProbablePrime(40))
+        .size
+
+  }
+
+}
---
+++ 27/src/test/scala/Problem27Test.scala
@@ -0,0 +1,9 @@
+class Problem27Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(-59231) {
+      Problem27.answer
+    }
+  }
+
+}
