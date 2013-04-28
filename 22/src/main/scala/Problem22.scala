12a518ade7f04ecac42b67c28ece187a9efe5ae9
Christopher Martin <ch.martin@gmail.com>
2013-04-28 03:10:03 -0400

    refactor 22

---
+++ 22/src/main/scala/Problem22.scala
@@ -0,0 +1,15 @@
+object Problem22 extends App {
+
+  println(answer)
+
+  def answer: Int =
+    io.Source.fromURL(getClass.getResource("names.txt"))
+      .getLines.mkString.toUpperCase.split(",")
+      .map(_.trim.stripPrefix("\"").stripSuffix("\""))
+      .sorted
+      .map(name => name.map(_ - 'A' + 1).sum)
+      .zip(Stream.from(1))
+      .map(x => x._1 * x._2)
+      .sum
+
+}
---
+++ 22/src/test/scala/Problem22Test.scala
@@ -0,0 +1,9 @@
+class Problem22Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(871198282) {
+      Problem22.answer
+    }
+  }
+
+}
