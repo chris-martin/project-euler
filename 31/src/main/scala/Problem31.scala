b5e944072bb2c5b0cc19cb9fb41f701bfc4eabef
Christopher Martin <ch.martin@gmail.com>
2013-04-28 04:26:41 -0400

    refactor 31

---
+++ 31/src/main/scala/Problem31.scala
@@ -0,0 +1,32 @@
+object Problem31 extends App {
+
+  println(answer)
+
+  def answer: Int = {
+
+    val denominations = Seq(1, 2, 5, 10, 20, 50, 100, 200)
+
+    val target = 200
+
+    implicit class Purse(quantities: Seq[Int] = Nil) {
+      def pence: Int = (denominations zip quantities).map(x => x._1 * x._2).sum
+    }
+
+    def count(base: Seq[Int] = Nil): Int = {
+
+      val p = base.pence
+
+      if (p == target)
+        1
+      else if (p > target)
+        0
+      else if (base.size == denominations.size)
+        0
+      else
+        (0 to (target-p)/denominations(base.size)).map({ n => count(base :+ n) }).sum
+    }
+
+    count()
+  }
+
+}
---
+++ 31/src/test/scala/Problem31Test.scala
@@ -0,0 +1,9 @@
+class Problem31Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(73682) {
+      Problem31.answer
+    }
+  }
+
+}
