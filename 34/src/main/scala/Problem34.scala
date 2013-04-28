e88beaad4dbeac31feb1054dcea1a42ebafe64e8
Christopher Martin <ch.martin@gmail.com>
2013-04-28 04:42:00 -0400

    refactor 34

--- 34/src/main/scala/Problem34.scala
+++ 34/src/main/scala/Problem34.scala
@@ -1,17 +1,18 @@
-object Main extends App {
+object Problem34 extends App {
+
+  println(answer)
 
   lazy val factorials: Stream[BigInt] =
     BigInt(1) #:: Stream.from(1).zip(factorials).map(x => x._2 * x._1)
 
-  val maxDigits = Stream.from(1).takeWhile({ i =>
+  lazy val maxDigits = Stream.from(1).takeWhile({ i =>
     (factorials(9) * i) >= (BigInt(10) pow i)
   }).last
 
   def curious(n: Int): Boolean =
     n == n.toString.map(_.toString.toInt).map(factorials(_)).sum
 
-  println(
+  def answer: Int =
     (3 to BigInt(10).pow(maxDigits).toInt).filter(curious(_)).sum
-  )
 
 }
---
+++ 34/src/test/scala/Problem34Test.scala
@@ -0,0 +1,9 @@
+class Problem34Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(40730) {
+      Problem34.answer
+    }
+  }
+
+}
