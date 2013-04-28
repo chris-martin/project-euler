b64ad16952a3da1601c2fec3fdca71c1798c7895
Christopher Martin <ch.martin@gmail.com>
2013-04-28 02:01:04 -0400

    refactor 12

---
+++ 12/src/main/scala/Problem12.scala
@@ -0,0 +1,25 @@
+object Problem12 extends App {
+
+  println(answer)
+
+  lazy val primes: Stream[BigInt] =
+    Stream.from(2).map(BigInt(_)).filter(_.isProbablePrime(45))
+
+  lazy val triangles: Stream[BigInt] = BigInt(0) #::
+    triangles.zip(Stream.from(1)).map(x => x._1 + x._2)
+
+  def nrOfFactors(n: BigInt): Int = {
+    val fs = new collection.mutable.HashMap[BigInt, Int]
+    var i = n
+    while (i != 1) {
+      val p = primes.find(p => i % p == 0).get
+      fs.put(p, fs.getOrElse(p, 0) + 1)
+      i /= p
+    }
+    fs.values.map(1 + _).product
+  }
+
+  def answer: Int =
+    triangles.drop(1).filter(nrOfFactors(_) > 500).head.toInt
+
+}
---
+++ 12/src/test/scala/Problem12Test.scala
@@ -0,0 +1,9 @@
+class Problem12Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(76576500) {
+      Problem12.answer
+    }
+  }
+
+}
