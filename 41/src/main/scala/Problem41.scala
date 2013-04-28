0b4c833e7ad8091ec50822c56177ae775adffc45
Christopher Martin <ch.martin@gmail.com>
2013-04-28 05:08:46 -0400

    refactor 41

--- 41/src/main/scala/Problem41.scala
+++ 41/src/main/scala/Problem41.scala
@@ -1,9 +1,13 @@
-object Main extends App {
-  println(
+object Problem41 extends App {
+
+  println(answer)
+
+  def answer: Int =
     (1 to 9)
       .flatMap(i => (1 to i).permutations)
       .map(_.mkString).sorted.reverse
       .filter(BigInt(_).isProbablePrime(40))
       .head
-  )
+      .toInt
+
 }
---
+++ 41/src/test/scala/Problem41Test.scala
@@ -0,0 +1,9 @@
+class Problem41Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(7652413) {
+      Problem41.answer
+    }
+  }
+
+}
