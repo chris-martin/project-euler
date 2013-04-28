c37d86cac3f9fcf05711a99f6cd375b1ce02a5c3
Christopher Martin <ch.martin@gmail.com>
2013-04-28 04:22:33 -0400

    refactor 30

---
+++ 30/src/main/scala/Problem30.scala
@@ -0,0 +1,30 @@
+object Problem30 extends App {
+
+  Seq(
+    "Numbers: %s".format(magicNrs.mkString(", ")),
+    "Sum: %s".format(answer)
+  ).foreach(println(_))
+
+  case class NrOfDigits(n: Int) {
+    def maxPowerSum: Int = n * BigInt(9).pow(5).toInt
+    def minValue: Int = BigInt(10).pow(n-1).toInt
+    def isFeasible: Boolean = maxPowerSum >= minValue
+  }
+
+  lazy val maxNrOfDigits =
+    Stream.from(1)
+      .map(NrOfDigits(_))
+      .takeWhile(_.isFeasible)
+      .last
+
+  def isMagic(n: Int): Boolean =
+    n == n.toString.map({ c =>
+      BigInt(c.toString) pow 5
+    }).sum
+
+  lazy val magicNrs =
+    (2 to maxNrOfDigits.maxPowerSum).filter(isMagic(_))
+
+  lazy val answer = magicNrs.sum
+
+}
---
+++ 30/src/test/scala/Problem30Test.scala
@@ -0,0 +1,9 @@
+class Problem30Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(443839) {
+      Problem30.answer
+    }
+  }
+
+}
