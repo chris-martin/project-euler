7a1d68e1023c1f168ea57483ab7167c5a3691112
Christopher Martin <ch.martin@gmail.com>
2013-04-28 04:45:03 -0400

    refactor 35

---
+++ 35/src/main/scala/Problem35.scala
@@ -0,0 +1,14 @@
+object Problem35 extends App {
+
+  println(answer)
+
+  def answer: Int =
+    (2 to 999999) count { x =>
+      val s = x.toString
+      (0 to s.size) forall { i =>
+        val t = s.substring(i) + s.substring(0, i)
+        BigInt(t).isProbablePrime(40)
+      }
+    }
+
+}
---
+++ 35/src/test/scala/Problem35Test.scala
@@ -0,0 +1,9 @@
+class Problem35Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(55) {
+      Problem35.answer
+    }
+  }
+
+}
