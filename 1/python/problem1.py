cdbaa9b774ff0f26c915a5756b5bcc5092f8b503
Christopher Martin <ch.martin@gmail.com>
2013-05-25 22:27:54 -0400

    problem 1: python 3, test scripts for everyone

--- 1/python/problem1.py
+++ 1/python/problem1.py
@@ -1,2 +1,2 @@
 def answer():
-  return sum(i for i in xrange(1, 1000) if i % 3 == 0 or i % 5 == 0)
+  return sum(i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0)
---
+++ 1/python/test.sh
@@ -0,0 +1,1 @@
+python3 problem1-test.py
