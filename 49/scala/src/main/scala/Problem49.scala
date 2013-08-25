a8ce68d20fdebbbbb2359a1578a8a3b53c9dc231
Christopher Martin <ch.martin@gmail.com>
2013-08-25 15:06:03 -0400

    problem 49

---
+++ 49/scala/src/main/scala/Problem49.scala
@@ -0,0 +1,11 @@
+object Problem49 extends App {
+
+  lazy val answer =
+    (1000 to 9999).filter(BigInt(_).isProbablePrime(32))
+    .groupBy(_.toString.sorted.mkString).values
+    .flatMap(_.sorted.combinations(3))
+    .filter(x => x(2) - x(1) == x(1) - x(0))
+    .filterNot(_ == Seq(1487, 4817, 8147))
+    .head.mkString
+
+}
