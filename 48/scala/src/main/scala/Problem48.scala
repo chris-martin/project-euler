29ca6945825d51c7f0ce2adea7c4b04b42d47110
Christopher Martin <ch.martin@gmail.com>
2013-08-25 14:26:51 -0400

    problem 48

---
+++ 48/scala/src/main/scala/Problem48.scala
@@ -0,0 +1,5 @@
+object Problem48 {
+
+  lazy val answer = (1 to 1000).map(i => BigInt(i) pow i).sum.toString.takeRight(10).mkString.toLong
+
+}
---
+++ 48/scala/src/test/scala/Problem48Test.scala
@@ -0,0 +1,9 @@
+class Problem48Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(9110846700l) {
+      Problem48.answer
+    }
+  }
+
+}
