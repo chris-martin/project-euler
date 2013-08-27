f29130d1b30e48ad17273be80ba8c9fea57665d3
Christopher Martin <ch.martin@gmail.com>
2013-08-26 23:01:43 -0400

    problem 56

---
+++ 56/scala/src/main/scala/Problem56.scala
@@ -0,0 +1,8 @@
+object Problem56 {
+
+  lazy val answer: Int = (
+    for (a <- 1 to 99; b <- 1 to 99)
+    yield (BigInt(a) pow b).toString.map(_.toString.toInt).sum
+  ).max
+
+}
---
+++ 56/scala/src/test/scala/Problem56Test.scala
@@ -0,0 +1,9 @@
+class Problem56Test extends org.scalatest.FreeSpec {
+
+  "Answer is correct" in {
+    expectResult(972) {
+      Problem56.answer
+    }
+  }
+
+}
