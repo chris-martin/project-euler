4921f5fecd5c114fdf906e111a4316e2d95d601c
Christopher Martin <ch.martin@gmail.com>
2014-01-05 01:56:37 -0500

    60 - second attempt
    150 seconds, wrong answer :(

--- 60/scala/src/main/scala/Problem60.scala
+++ 60/scala/src/main/scala/Problem60.scala
@@ -2,52 +2,59 @@ import collection.mutable, mutable.ArrayBuffer
 
 object Problem60 {
 
-  object Primes {
-
-    private val primeStream = Stream.from(2).filter(BigInt(_).isProbablePrime(30))
-    private val primeSet = mutable.Set[Int]()
-    private var unsearched: Stream[Int] = primeStream
-
-    private def generateTo(i: Int) {
-      while (unsearched.head <= i + 10000) {
-        val i = unsearched.head
-        unsearched = unsearched.tail
-        primeSet add i
-      }
-    }
-
-    def test(i: Int): Boolean =
-      { generateTo(i); primeSet contains i }
-
-    def iterator: Iterator[Int] = primeStream.iterator
-  }
+  val primality = mutable.Map[Long, Boolean]().withDefault(BigInt(_).isProbablePrime(30))
 
   // Graph: vertices are primes, edges are present where the concatenation property holds.
   // Find the 5-clique with the lowest sum.
 
-  def isEdge(x: String, y: String): Boolean =
-    Primes.test((x + y).toInt) &&
-    Primes.test((y + x).toInt)
+  def isEdge(x: Long, y: Long): Boolean =
+    isEdge(x.toString, y.toString)
 
-  def smallestCliqueOfSize(n: Int): Set[Int] = {
-    val cliques = ArrayBuffer[ArrayBuffer[String]]()
-    for (p <- Primes.iterator.map(_.toString)) {
-      println(p)
+  def isEdge(x: String, y: String): Boolean =
+    primality((x + y).toLong) &&
+    primality((y + x).toLong)
+
+  class Clique(vs: ArrayBuffer[Long]) {
+    def sum: Long = vs.sum
+    def accepts(p: Long): Boolean = vs forall (isEdge(p, _))
+    def +=(p: Long) { vs += p }
+    def size: Int = vs.size
+    def toSet: Set[Long] = vs.toSet
+  }
+  object Clique {
+    def apply(ps: Long*): Clique = new Clique(ArrayBuffer(ps: _*))
+  }
+
+  class HashCodeOrdering[A] extends Ordering[A] {
+    def compare(x: A, y: A) = x.hashCode - y.hashCode
+  }
+
+  def smallestCliqueOfSize(n: Int, maxSum: Long): Option[Set[Long]] = {
+
+    println(n)
+
+    val cliques = mutable.TreeSet[Clique]()(new HashCodeOrdering)
+    var result: Option[Clique] = None
+
+    for (p <- Iterator.iterate(2L)(_+1).takeWhile(_ <= maxSum).filter(primality)) {
       for (clique <- cliques) {
-        if (clique forall (isEdge(p, _))) {
+        if (clique.sum + p >= maxSum) {
+          cliques -= clique
+        }
+        if (clique accepts p) {
           clique += p
           if (clique.size == n) {
-            return clique.map(_.toInt).toSet
+            result = Some((Seq(clique) ++ result).minBy(_.sum))
           }
         }
       }
-      cliques += ArrayBuffer(p)
+      cliques += Clique(p)
     }
-    throw new AssertionError
+    result.map(_.toSet)
   }
 
-  lazy val smallestCliqueOfSize4 = smallestCliqueOfSize(4)
-  lazy val smallestCliqueOfSize5 = smallestCliqueOfSize(5)
+  lazy val smallestCliqueOfSize4 = smallestCliqueOfSize(4, 1000)
+  lazy val smallestCliqueOfSize5 = smallestCliqueOfSize(5, 110000)
 
-  lazy val answer: Int = smallestCliqueOfSize(5).sum
+  lazy val answer: Long = smallestCliqueOfSize5.get.sum
 }
--- 60/scala/src/test/scala/Problem60Test.scala
+++ 60/scala/src/test/scala/Problem60Test.scala
@@ -2,18 +2,15 @@ class Problem60Test extends org.scalatest.FreeSpec {
 
   import Problem60._
 
-  /*"Size 4" in {
-    println("4")
-    assert ( smallestCliqueOfSize4 === Set(3, 7, 109, 673) )
-  }*/
-
-  "Size 5" in {
-    println("5")
-    assert ( smallestCliqueOfSize5 === Set() )
+  "Size 4" in {
+    assert ( smallestCliqueOfSize4 === Some(Set(3, 7, 109, 673)) )
   }
 
-  /*"Answer is correct" in {
-    println("a")
-    assert ( answer === 0 )
-  }*/
+  "Size 5" in {
+    assert ( smallestCliqueOfSize5 === Some(Set(5381, 5507, 7877, 41621, 47237)) )
+  }
+
+  "Answer is correct" in {
+    assert ( answer === 107623 )
+  }
 }
