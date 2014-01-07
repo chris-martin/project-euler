d5a47c64776976b47da1b7f6257e0edca1012290
Christopher Martin <ch.martin@gmail.com>
2014-01-07 01:39:05 -0500

    66 - same wrong answer, but simpler

--- 66/scala/src/main/scala/Problem66.scala
+++ 66/scala/src/main/scala/Problem66.scala
@@ -4,62 +4,51 @@ object Problem66 {
 
   lazy val answer: Int = dMaximizingX(maxD = 1000)
 
-  def dMaximizingX(maxD: Int): Int = {
+  object Roots {
 
-    val solutions = mutable.Map[Int, (Int, Int)]() // D -> (x, y)
+    val map = mutable.Map[Long, Int]()
+    var maxSquare = 0L
+    var maxRoot = 0
+
+    def get(x: Long): Option[Int] = {
+      while (maxSquare < x) {
+        maxRoot += 1
+        maxSquare = maxRoot * maxRoot
+        map put (maxSquare, maxRoot)
+      }
+      map get x
+    }
+  }
+
+  def dMaximizingX(maxD: Int): Int = {
 
     val unsolvedD = mutable.Set[Int]((
       (1 to maxD).toSet &~
         Stream.from(1).map(x => x*x).takeWhile(_ <= maxD).toSet
     ).toSeq :_*)
 
-    val roots = (2 to maxD).map(d => (d, math.sqrt(d))).toMap
-
     var x = 2
-    var xSquaredMinusOne = 3
-    var sqrtXSquaredMinusOne = math.sqrt(xSquaredMinusOne)
-    var xSquared = 4
+    var lastD = 0
     while (unsolvedD.nonEmpty) {
       for (d <- unsolvedD) {
-        solveForY(x = x, d = d,
-          xSquared = xSquared,
-          sqrtXSquaredMinusOne = sqrtXSquaredMinusOne,
-          sqrtD = roots(d)
-        ) foreach { y =>
-          solutions put (d, (x, y))
+        solveForY(x = x, d = d) foreach { y =>
+          lastD = d
           unsolvedD -= d
-          println(solutions.size + "       " + (d, (x, y)))
+          println(s"${unsolvedD.size}\td = $d  \tx = $x\ty = $y")
         }
       }
-      xSquaredMinusOne = xSquared + x + x
-      xSquared = xSquaredMinusOne + 1
       x += 1
-      sqrtXSquaredMinusOne = math.sqrt(xSquaredMinusOne)
     }
 
-    solutions.maxBy(_._2._1)._1
+    lastD
   }
 
-  def solveForD(x: Int, y: Int): Option[Int] = {
-    val d = ((x.toDouble*x - 1) / (y*y)).round.toInt
-    if (x*x - d*y*y == 1) Some(d.toInt) else None
-  }
+  def solveForY(x: Int, d: Int): Option[Int] =
+    divide(x*x - 1, d) flatMap (Roots.get(_))
 
-  def solveForY(x: Int, d: Int): Option[Int] = {
-    val y = math.sqrt((x.toDouble*x - 1) / d).round.toInt
-    if (x*x - d*y*y == 1) Some(y.toInt) else None
-  }
-
-  def solveForY(x: Int, d: Int,
-    xSquared: Int,
-    sqrtXSquaredMinusOne: Double,
-    sqrtD: Double
-  ): Option[Int] = {
-    val y = ( sqrtXSquaredMinusOne / sqrtD ).round.toInt
-    if (xSquared - d*y*y == 1) Some(y.toInt) else None
-  }
-
-  case class Equation(x: Int, y: Int, d: Int) {
-    def test = x*x - d*y*y == 1
-  }
+  def divide(a: Int, b: Int): Option[Int] =
+    BigInt(a) /% b match {
+      case (q, r) if r.toInt == 0 => Some(q.toInt)
+      case _ => None
+    }
 }
--- 66/scala/src/test/scala/Problem66Test.scala
+++ 66/scala/src/test/scala/Problem66Test.scala
@@ -2,14 +2,6 @@ class Problem66Test extends org.scalatest.FreeSpec {
 
   import Problem66._
 
-  "solve for d" - {
-    "3^2 – 2 × 2^2 = 1" in { assert ( solveForD(3, 2) === Some(2) ) }
-    "2^2 – 3 × 1^2 = 1" in { assert ( solveForD(2, 1) === Some(3) ) }
-    "9^2 – 5 × 4^2 = 1" in { assert ( solveForD(9, 4) === Some(5) ) }
-    "5^2 – 6 × 2^2 = 1" in { assert ( solveForD(5, 2) === Some(6) ) }
-    "8^2 – 7 × 3^2 = 1" in { assert ( solveForD(8, 3) === Some(7) ) }
-  }
-
   "solve for y" - {
     "3^2 – 2 × 2^2 = 1" in { assert ( solveForY(3, 2) === Some(2) ) }
     "2^2 – 3 × 1^2 = 1" in { assert ( solveForY(2, 3) === Some(1) ) }
