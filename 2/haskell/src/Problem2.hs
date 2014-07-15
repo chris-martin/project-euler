773c94c79446a82cb138214197ddb97e8a2eb8e3
Chris Martin <ch.martin@gmail.com>
2014-07-15 04:26:06 -0400

    cleanup 2/haskell

--- 2/haskell/src/Problem2.hs
+++ 2/haskell/src/Problem2.hs
@@ -1,14 +1,8 @@
 module Problem2 where
 
 answer :: Integer
-answer = fibs
-  # takeWhile(< 4000000)
-  # filter(\x -> x `mod` 2 == 0)
-  # sum
-
-fibs :: [Integer]
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-infixl 8 #
-(#) :: a -> (a -> b) -> b
-a # f = f a
+answer = sum $ filter even $ takeWhile (< bound) fibs
+  where
+    fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+    bound = 4 * million
+    million = 10 ^ (6 :: Integer)
