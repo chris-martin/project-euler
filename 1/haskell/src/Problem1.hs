506d60d217ee6be18d1dbe5dca00c665a990dde8
Chris Martin <ch.martin@gmail.com>
2014-08-16 17:02:45 -0400

    minor change to 1/haskell

--- 1/haskell/src/Problem1.hs
+++ 1/haskell/src/Problem1.hs
@@ -4,5 +4,5 @@ answer :: Integer
 answer = sum multiples
   where
     multiples = filter isMultiple [1..999]
-    isMultiple (n) = any (`divides` n) [3,5]
+    isMultiple n = any (`divides` n) [3, 5]
     d `divides` n = n `mod` d == 0
