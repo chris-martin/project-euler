d7c5b66740393d1f46bb0722a6940478ed0e6c9f
Chris Martin <ch.martin@gmail.com>
2015-12-14 03:52:29 -0800

    scala 9 cleanup

--- 009/scala/src/main/scala/Problem9.scala
+++ 009/scala/src/main/scala/Problem9.scala
@@ -1,19 +1,13 @@
 object Problem9 {
 
+  def square(n: Int) = n * n
+
   lazy val answer: Int =
-    (
-      for (
-        a <- (0 to 1000).toStream;
-        b <- ((a + 1) to 1000).toStream
-      ) yield {
-
-        val c = 1000 - (a + b)
-
-        if (b < c && a*a + b*b == c*c)
-          Some(Seq(a, b, c))
-        else
-          None
-      }
-    ).view.flatten.head.product
+    (for {
+      a <- 0 to 1000
+      b <- 0 to a
+      c = 1000 - (a + b)
+      if square(a) + square(b) == square(c)
+    } yield a * b * c).head
 
 }
