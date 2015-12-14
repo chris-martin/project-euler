a768c225eecef41f722a70b66b23d96592279891
Chris Martin <ch.martin@gmail.com>
2015-12-14 01:16:32 -0800

    scala 5: cleanup

--- 005/scala/src/main/scala/Problem5.scala
+++ 005/scala/src/main/scala/Problem5.scala
@@ -1,12 +1,8 @@
 object Problem5 {
 
-  lazy val answer: Int = (
-    Stream from 1
-      filter { a =>
-        (3 to 20) forall {
-          b => a % b == 0
-        }
-      }
-    ).head
+  lazy val answer: Int =
+    Stream.from(1)
+      .filter(a => (3 to 20).forall(b => a % b == 0))
+      .head
 
 }
