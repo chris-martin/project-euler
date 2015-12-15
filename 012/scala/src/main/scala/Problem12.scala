90b5c91d05641e3631331ce60c0e71cedfe88db6
Chris Martin <ch.martin@gmail.com>
2015-12-15 01:28:06 -0800

    scala 12 cleanup

--- 012/scala/src/main/scala/Problem12.scala
+++ 012/scala/src/main/scala/Problem12.scala
@@ -1,15 +1,19 @@
 object Problem12 {
 
-  lazy val primes: Stream[BigInt] =
-    Stream.from(2).map(BigInt(_)).filter(_.isProbablePrime(45))
+  println("hi")
 
-  lazy val triangles: Stream[BigInt] = BigInt(0) #::
-    triangles.zip(Stream.from(1)).map(x => x._1 + x._2)
+  lazy val primes: Stream[BigInt] =
+    Stream.iterate(BigInt(2))(_ + 1)
+      .filter(_.isProbablePrime(45))
+
+  lazy val triangles: Stream[BigInt] =
+    Stream.iterate(BigInt(1))(_ + 1)
+      .scanLeft(BigInt(0))(_ + _)
 
   def nrOfFactors(n: BigInt): Int = {
     val fs = new collection.mutable.HashMap[BigInt, Int]
     var i = n
-    while (i != 1) {
+    while (i != BigInt(1)) {
       val p = primes.find(p => i % p == 0).get
       fs.put(p, fs.getOrElse(p, 0) + 1)
       i /= p
