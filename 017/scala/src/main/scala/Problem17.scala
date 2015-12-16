8949097eb81c58a39645f0805aa1efb2145abc11
Chris Martin <ch.martin@gmail.com>
2015-12-16 02:46:41 -0800

    scala 17 cleanup

--- 017/scala/src/main/scala/Problem17.scala
+++ 017/scala/src/main/scala/Problem17.scala
@@ -1,36 +1,36 @@
 object Problem17 {
 
-  lazy val answer: Int = strings.map(_.size).sum
+  lazy val answer: Int = strings.map(_.length).sum
 
   lazy val strings: Seq[String] = {
 
-    val lows = """one two three four five six seven eight nine ten
-      eleven twelve thirteen fourteen fifteen sixteen seventeen
-      eighteen nineteen""".trim.split("""\s+""").map(_.capitalize)
-    assert(lows.size == 19)
+    val `1-9` = Seq("one", "two", "three", "four", "five",
+                    "six", "seven", "eight", "nine")
 
-    val oneDigit = lows.take(9)
-    assert(oneDigit.size == 9)
+    val `10-19` = Seq("ten", "eleven", "twelve", "thirteen",
+                      "fourteen", "fifteen", "sixteen", "seventeen",
+                      "eighteen", "nineteen")
 
-    val tens = "twenty thirty forty fifty sixty seventy eighty ninety".
-      split(' ').map(_.capitalize)
-    assert(tens.size == 8)
+    val `20-99` =
+      for {
+        a <- Seq("twenty", "thirty", "forty", "fifty",
+                 "sixty", "seventy", "eighty", "ninety")
+        b <- "" +: `1-9`
+      } yield a + b
 
-    val oneAndTwoDigit = lows ++ (
-      for (a <- tens; b <- Seq("") ++ lows.take(9))
-      yield (a + b)
-    )
-    assert(oneAndTwoDigit.size == 99)
+    val `1-99` = `1-9` ++ `10-19` ++ `20-99`
 
-    val threeDigit = (
-      for (a <- oneDigit; b <- Seq("") ++ oneAndTwoDigit.map("And" + _))
-      yield (a + "Hundred" + b)
-    )
-    assert(threeDigit.size == 900)
+    val `100-999` =
+      for {
+        a <- `1-9`
+        b <- "" +: `1-99`.map("and" + _)
+      } yield a + "hundred" + b
 
-    val all = oneAndTwoDigit ++ threeDigit :+ "OneThousand"
-    assert(all.size == 1000)
-    all
+    val `1-1000` = `1-99` ++ `100-999` :+ "onethousand"
+
+    assert(`1-1000`.size == 1000)
+
+    `1-1000`
   }
 
 }
--- 017/scala/src/test/scala/Problem17Test.scala
+++ 017/scala/src/test/scala/Problem17Test.scala
@@ -1,12 +1,5 @@
 class Problem17Test extends org.scalatest.FreeSpec {
 
-  "Strings" in {
-    Problem17.strings
-      .grouped(100)
-      .map(_.mkString(" "))
-      .foreach(info(_))
-  }
-
   "Answer is correct" in {
     expectResult(21124) {
       Problem17.answer
