ee0e25b5491f533b492dab48d2129e9068314d14
Chris Martin <ch.martin@gmail.com>
2015-12-16 01:49:27 -0800

    clean up clojure 15

--- 015/clojure/src/Problem15/answer.clj
+++ 015/clojure/src/Problem15/answer.clj
@@ -1,28 +1,32 @@
 (ns Problem15.answer
   (:require [clojure.core.match :refer [match]]))
 
-(defn right [dim] (let [[x y] dim] [(- x 1) y]))
-(defn down [dim] (let [[x y] dim] [x (- y 1)]))
-
 (defn count-paths [counts stack]
-  (cond (empty? stack) counts
-        (contains? counts (first stack)) (recur counts (rest stack))
-        :else (let [dim (first stack)]
-                (match [dim
-                        [(contains? counts (right dim))
-                         (contains? counts (down dim))]]
-                       [[0 y] _] (recur (assoc counts dim 1) (rest stack))
-                       [[x 0] _] (recur (assoc counts dim 1) (rest stack))
-                       [_ [true true]] (recur (assoc counts dim (+ (get counts (right dim))
-                                                                   (get counts (down dim))))
-                                              (rest stack))
-                       [_ [true false]] (recur counts (->> stack
-                                                           (cons (down dim))))
-                       [_ [false true]] (recur counts (->> stack
-                                                           (cons (right dim))))
-                       [_ [false false]] (recur counts (->> stack
-                                                            (cons (down dim))
-                                                            (cons (right dim))))))))
+  (let [xy (first stack)
+        [x y] xy
+        adjacencies (if (nil? xy) nil [[(- x 1) y]
+                                       [x (- y 1)]])
+        unknown-adjacencies (filter #(not (contains? counts %)) adjacencies)]
+    (cond
+
+      ; When the stack is empty, we're done.
+      (empty? stack) counts
+
+      ; If the stack head's count is already known, just pop it off.
+      (contains? counts xy) (recur counts (rest stack))
+
+      ; If either coordinate is zero, there's only one path.
+      ; Add it to the counts and pop it off the stack,
+      (some #{0} xy) (recur (assoc counts xy 1) (rest stack))
+
+      ; If both of the adjacencies' counts are known, then this count
+      ; is their sum. Add it to the counts and pop it off the stack,
+      (empty? unknown-adjacencies)
+      (let [count (apply + (map (partial get counts) adjacencies))]
+        (recur (assoc counts xy count) (rest stack)))
+
+      ; There are some unknown adjacencies. Add them to the stack.
+      :else (recur counts (concat unknown-adjacencies stack)))))
 
 (defn answer []
   (let [d [20 20]]
