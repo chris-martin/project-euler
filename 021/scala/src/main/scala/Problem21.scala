e8cde54951d98361d54fd875c709e7d6117a3859
Chris Martin <ch.martin@gmail.com>
2015-12-16 20:04:33 -0800

    scala 21 cleanup

--- 021/scala/src/main/scala/Problem21.scala
+++ 021/scala/src/main/scala/Problem21.scala
@@ -1,7 +1,6 @@
 object Problem21 {
 
-  lazy val answer: Int =
-    (2 to max).filter(isAmicable(_)).sum
+  lazy val answer: Int = (2 to max).filter(isAmicable).sum
 
   lazy val max = 9999
 
@@ -15,25 +14,22 @@ object Problem21 {
     primes.map(f => f :: tail match {
       case fs if fs.product > max => None
       case fs => Some(Stream.cons(fs, factorizations(fs)))
-    }).takeWhile(_.isDefined).map(_.get).flatten
+    }).takeWhile(_.isDefined).flatMap(_.get)
 
   // the proper divisors of the product of the factors
-  def properDivisors(fs: Factors): Set[Int] = {
-    (0 until fs.size).toSet.flatMap { n: Int =>
+  def properDivisors(fs: Factors): Set[Int] =
+    fs.indices.toSet.flatMap { n: Int =>
       fs.combinations(n).map(_.product)
     }
-  }
 
   // function : n in [2, max] -> Some(sum of proper divisors of n)
   lazy val divisorSums = {
     val ds = new Array[Int](max + 1)
-    for (fs <- factorizations()) {
+    for (fs <- factorizations())
       ds.update(fs.product, properDivisors(fs).sum)
-    }
     i: Int => if (i >= 2 && i <= max) Some(ds(i)) else None
   }
 
   def isAmicable(i: Int): Boolean =
-    divisorSums(i).map(x => x != i && divisorSums(x) == Some(i)).getOrElse(false)
-
+    divisorSums(i).exists(x => x != i && divisorSums(x) == Some(i))
 }
