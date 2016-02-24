40cf366f811138578c8254918811e3e6a6b4955f
Chris Martin <ch.martin@gmail.com>
2016-02-24 04:54:46 -0800

    haskell: add docs to Collatz

--- haskell/src/Euler/Util/Collatz.hs
+++ haskell/src/Euler/Util/Collatz.hs
@@ -1,18 +1,26 @@
--- | The Collatz sequence is defined for the set of positive integers:
---
--- * /n → n\/2/ (/n/ is even)
--- * /n → 3n + 1/ (/n/ is odd)
-
-module Euler.Util.Collatz (collatz, collatzLengths) where
+module Euler.Util.Collatz
+    ( collatz
+    , collatzLengths
+    , Lengths
+    ) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+-- | One step of the Collatz sequence, defined for the set of
+-- positive integers:
+--
+-- * /n → n\/2/ (/n/ is even)
+-- * /n → 3n + 1/ (/n/ is odd)
 collatz :: Integer -> Integer
 collatz i = if even i then i `div` 2 else 3 * i + 1
 
+-- | A mapping from /n/ to the length of the Collatz sequence starting
+-- from /n/.
 type Lengths = Map Integer Integer
 
+-- | @'collatzLengths' xs@ evaluates to a 'Lengths' mapping which contains
+-- the collatz length for at least every element of @xs@ (and possibly more).
 collatzLengths :: [Integer] -> Lengths
 collatzLengths = getLengths $ Map.singleton 1 1
 
