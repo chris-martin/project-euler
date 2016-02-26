435764f2562b23ff6785f20dbccd6451571a9b85
Chris Martin <ch.martin@gmail.com>
2016-02-26 01:54:37 -0800

    travis: fix site deploy

--- .site/deploy.sh
+++ .site/deploy.sh
@@ -13,4 +13,4 @@ git commit --message "Deploy to GitHub Pages"
 GITHUB_URL=https://$GITHUB_TOKEN@github.com/chris-martin/project-euler.git
 
 # Redirect any output to /dev/null to avoid exposing the GitHub access token.
-git push --force --quiet $GITHUB_URL master:gh-pages > /dev/null 2>&1
+git push --force --quiet $GITHUB_URL HEAD:gh-pages > /dev/null 2>&1
