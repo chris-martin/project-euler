09d7cce7d1eed7ae40aaabb1bc78334d5febe2e5
Chris Martin <ch.martin@gmail.com>
2016-02-26 03:27:59 -0800

    add tri/hex to figurate numbers

--- haskell/src/Euler/Util/FigurateNumbers.hs
+++ haskell/src/Euler/Util/FigurateNumbers.hs
@@ -1,26 +1,81 @@
 -- | <https://en.wikipedia.org/wiki/Figurate_number Figurate numbers>
 -- are sets of numbers related to polygon shapes.
 module Euler.Util.FigurateNumbers
-    ( pentagonals
+    (
+    -- * Triangle
+      triangles
+    , triangleN
+    , isTriangle
+
+    -- * Pentagonal
+    , pentagonals
     , pentagonalN
     , isPentagonal
+
+    -- * Hexagonal
+    , hexagonals
+    , hexagonalN
+    , isHexagonal
+
     ) where
 
 import qualified Euler.Util.Polynomial as Poly
 
--- | All of the pentagonals /P_1, P_2, .../
+-----------------------------------------------------------
+--  Triangle
+-----------------------------------------------------------
+
+-- | All of the triangle numbers, ascending
+triangles :: Integral a => [a]
+triangles = map triangleN [1..]
+
+-- | @'triangleN' n@ is the /n/th triangle number
+triangleN :: Integral a => a -> a
+triangleN n = (n * (n + 1)) `div` 2
+
+-- | @'isTriangle' x@ determines whether 'triangles' contains @x@,
+-- determined by testing whether the solution of /0 = -2x + n + n^2/
+-- is integral.
+isTriangle :: Integral a => a -> Bool
+isTriangle x = Poly.hasIntRootBetween [-2 * x, 1, 1] 0 x
+
+
+-----------------------------------------------------------
+--  Pentagonal
+-----------------------------------------------------------
+
+-- | All of the pentagonal numbers, ascending
 pentagonals :: Integral a => [a]
 pentagonals = map snd $
     iterate (\(n, p) -> (n + 1,
                          p + 3 * (n + 1) - 2))
             (1, 1)
 
--- | @'pentagonalN' n@ = /P_n/
+-- | @'pentagonalN' n@ is the /n/th pentagonal number
 pentagonalN :: Integral a => a -> a
-pentagonalN n = n * (3 * n - 1) `div` 2
+pentagonalN n = (n * (3 * n - 1)) `div` 2
 
--- | @'isPentagonal' p@ determines whether 'pentagonals' contains @p@,
--- determined by testing whether the solution of /0 = (3n^2 - n - 2p)/
+-- | @'isPentagonal' x@ determines whether 'pentagonals' contains @x@,
+-- determined by testing whether the solution of /0 = 3n^2 - n - 2x/
 -- is integral.
 isPentagonal :: Integral a => a -> Bool
-isPentagonal p = Poly.hasIntRootBetween [-2 * p, -1, 3] 0 p
+isPentagonal x = Poly.hasIntRootBetween [-2 * x, -1, 3] 0 x
+
+
+-----------------------------------------------------------
+--  Hexagonal
+-----------------------------------------------------------
+
+-- | All of the hexagonal numbers, ascending
+hexagonals :: Integral a => [a]
+hexagonals = map hexagonalN [1..]
+
+-- | @'hexagonalN' n@ is the /n/th hexagonal number
+hexagonalN :: Integral a => a -> a
+hexagonalN n = n * (2 * n - 1)
+
+-- | @'isHexagonal' x@ determines whether 'hexagonals' contains @x@,
+-- determined by testing whether the solution of /0 = -2x -n + 2n^2/
+-- is integral.
+isHexagonal :: Integral a => a -> Bool
+isHexagonal x = Poly.hasIntRootBetween [-x, -1, 2] 0 x
--- haskell/test/EulerTest/Util/FigurateNumbers.hs
+++ haskell/test/EulerTest/Util/FigurateNumbers.hs
@@ -7,13 +7,28 @@ import Test.Framework.Providers.HUnit (testCase)
 import Euler.Util.FigurateNumbers
 
 tests :: [Test]
-tests =
-  [ testGroup "pentagonals" $ concat
-    [ [ testCase "first handful" $
-          take 10 pentagonals @?= firstTenPentagonals
-      ]
-    , map isPentagonalTestN [1..100]
+tests = [triangleTest, pentagonalTest, hexagonalTest]
+
+triangleTest :: Test
+triangleTest = testGroup "triangles" $ concat
+  [ [ testCase "first handful" $
+      take 5 triangles @?= firstFiveTriangles
     ]
+  , map isTriangleTestN [1..20]
+  ]
+  where
+    firstFiveTriangles = [1, 3, 6, 10, 15]
+
+    isTriangleTestN n = testCase
+      ("isTriangle " ++ show n)
+      (isTriangle n @?= elem n firstFiveTriangles)
+
+pentagonalTest :: Test
+pentagonalTest = testGroup "pentagonals" $ concat
+  [ [ testCase "first handful" $
+      take 10 pentagonals @?= firstTenPentagonals
+    ]
+  , map isPentagonalTestN [1..100]
   ]
   where
     firstTenPentagonals = [1, 5, 12, 22, 35, 51, 70, 92, 117, 145]
@@ -21,3 +36,17 @@ tests =
     isPentagonalTestN n = testCase
       ("isPentagonal " ++ show n)
       (isPentagonal n @?= elem n firstTenPentagonals)
+
+hexagonalTest :: Test
+hexagonalTest = testGroup "hexagonals" $ concat
+  [ [ testCase "first handful" $
+      take 5 hexagonals @?= firstFiveHexagonals
+    ]
+  , map isHexagonalTestN [1..50]
+  ]
+  where
+    firstFiveHexagonals = [1, 6, 15, 28, 45]
+
+    isHexagonalTestN n = testCase
+      ("isHexagonal " ++ show n)
+      (isHexagonal n @?= elem n firstFiveHexagonals)
