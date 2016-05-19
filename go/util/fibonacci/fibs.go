62997c33d30d34982d058c73680bcc4713fc3279
Chris Martin <ch.martin@gmail.com>
2016-05-19 17:55:47 -0400

    remove comment - all go channels are bounded?

--- go/util/fibonacci/fibs.go
+++ go/util/fibonacci/fibs.go
@@ -1,9 +1,6 @@
 package fibonacci
 
 // Sends all of the fibonacci numbers to a channel.
-//
-// This is an unbounded sequence, so do not call this
-// this an unbounded channel!
 func Fibs(c chan int) {
     x, y := 1, 1
     for i := 0; true; i++ {
