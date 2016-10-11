bc30d7ecc85658ab00ced3f38c9405a3f8a03357
Chris Martin <ch.martin@gmail.com>
2016-10-11 17:11:12 -0400

    haskell 43 cleanup

--- haskell/src/Euler/Problems/Problem43.hs
+++ haskell/src/Euler/Problems/Problem43.hs
@@ -1,7 +1,5 @@
 module Euler.Problems.Problem43
-    ( answer
-    , substrings
-    ) where
+    ( answer, substrings, predicate ) where
 
 import Euler.Util.Arithmetic (divides)
 import Euler.Util.Digit      (digits, unDigits)
@@ -16,15 +14,17 @@ answer :: Integer
 predicate :: Integer -> Bool
 
 substrings :: Integral a => a -> [a]
--- ^ >>> substrings 1406357289
+-- ^ Three-digit substrings of the decimal representation
+-- of the input.
+--
+-- >>> substrings 1406357289
 -- [406,63,635,357,572,728,289]
 
 ---------------------------------------------------------
 
-answer = sum (filter predicate pandigitals)
+answer = sum $ filter predicate pandigitals
 
-predicate i = and (zipWith divides
-                    (primes :: [Integer])
-                    (substrings i))
+predicate i = and $ zipWith divides (primes :: [Integer])
+                                    (substrings i)
 
 substrings = map (unDigits 10) . drop 1 . sliding 3 . digits 10
