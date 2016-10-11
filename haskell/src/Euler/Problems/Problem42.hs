d627cb6f2154095aff67194c26716743b94159f7
Chris Martin <ch.martin@gmail.com>
2016-10-11 17:01:43 -0400

    haskell 42 cleanup

--- haskell/src/Euler/Problems/Problem42.hs
+++ haskell/src/Euler/Problems/Problem42.hs
@@ -1,16 +1,32 @@
-module Euler.Problems.Problem42 (answer) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Euler.Problems.Problem42
+    ( answer, parseWords, isTriangleWord ) where
 
 import           Data.Text (Text)
 import qualified Data.Text as Text
 
 import Euler.Util.FigurateNumbers (isTriangle)
-import Euler.Util.WordScore       (wordScore)
+import Euler.Util.WordScore       (wordScore, wordScoreText)
 
-answer :: Text -> Integer
+--------------------------------------------------------------------
+
+type InputText = Text
+-- ^ The contents of the input file: a comma-delimited
+--   list of quoted strings.
+
+answer :: InputText -> Integer
+-- ^ The number of triangle words in the input.
+
+parseWords :: InputText -> [Text]
+
+isTriangleWord :: Text -> Bool
+-- ^ Whether a word's score is a triangle number.
+
+--------------------------------------------------------------------
+
 answer = fromIntegral . length . filter isTriangleWord . parseWords
-  where isTriangleWord = isTriangle . wordScore
 
-parseWords :: Text -> [String]
-parseWords = map Text.unpack
-           . Text.splitOn (Text.pack ",")
-           . Text.filter (/= '"')
+parseWords = Text.splitOn "," . Text.filter (/= '"')
+
+isTriangleWord = isTriangle . wordScoreText
