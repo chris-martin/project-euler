2308c99c7b19c9b81962ae57954ff3eb34b8a2ce
Chris Martin <ch.martin@gmail.com>
2017-07-31 02:29:14 -0400

    bump up scala test limit

--- scala/src/test/scala/euler/problem/Test62.scala
+++ scala/src/test/scala/euler/problem/Test62.scala
@@ -7,7 +7,7 @@ import problem62._
 class Test62 extends org.scalatest.FreeSpec
 with TimeLimitedTests {
 
-  val timeLimit = Span(2000, Millis)
+  val timeLimit = Span(5000, Millis)
 
   "Three permutations" in assertResult(41063625)(permutationCubes(3))
 
