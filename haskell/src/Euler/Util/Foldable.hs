261df25b1e5fec638996790d093776784f035ab3
Chris Martin <ch.martin@gmail.com>
2018-10-03 22:07:36 -0400

    haskell build fixes

--- haskell/src/Euler/Util/Foldable.hs
+++ haskell/src/Euler/Util/Foldable.hs
@@ -21,19 +21,21 @@ data Aeq a = Aeq_nothing   -- ^ Empty
            | Aeq_different -- ^ Contains different elements
            | Aeq_same a    -- ^ Contains only this element
 
+instance Eq a => Semigroup (Aeq a)
+  where
+    (<>) (Aeq_same x) (Aeq_same y)
+            | x == y    = Aeq_same x
+            | otherwise = Aeq_different
+
+    (<>) Aeq_different _ = Aeq_different
+    (<>) _ Aeq_different = Aeq_different
+
+    (<>) Aeq_nothing x = x
+    (<>) x Aeq_nothing = x
+
 instance (Eq a) => Monoid (Aeq a)
   where
-
     mempty = Aeq_nothing
-
-    mappend (Aeq_same x) (Aeq_same y) | x == y    = Aeq_same x
-                                      | otherwise = Aeq_different
-
-    mappend Aeq_different _ = Aeq_different
-    mappend _ Aeq_different = Aeq_different
-
-    mappend Aeq_nothing x = x
-    mappend x Aeq_nothing = x
 
 aeq_different :: Aeq a -> Bool
 
